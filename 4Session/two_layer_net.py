b897825ad393071d4b0d376421bbb219fd339a6e
李繁 <lifan@Mr-Li-iMac.local>
2020-11-16 06:53:34 +0800

    Upload a implement for NetWork identify.

--- 4Session/two_layer_net.py
+++ 4Session/two_layer_net.py
@@ -3,6 +3,7 @@ import sys, os
 import numpy as np
 from common.functions import *
 from common.gradient import numerical_gradient
+from dataset.mnist import load_mnist
 
 
 class TwoLayerNet:
@@ -51,3 +52,44 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
+
+(x_train,t_train),(x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+train_loss_list = []
+# add some statistic
+train_acc_list = []
+test_acc_list = []
+# average the count of each epoch
+iter_per_epoch = max(train_size / batch_size, 1)
+
+# super para
+iter_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+network = TwoLayerNet(input_size = 784, hidden_size=50, output_size = 10)
+
+for i in range(iters_num):
+    # get mini-batch
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # calc gradient
+    grad= network.numerical_gradient(x_batch, t_batch)
+    # grad = network.gradient(x_batch, t_batch)  #high speed version
+
+    # update para
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key]-=learning_rate * grad[key]
+
+    # record learing process
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    #calc the accuracy of each epoch
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
